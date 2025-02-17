5a21aee3cc00d36767a6cc50c09a1ab83b78a704
Abhishek <abhikpujari7@gmail.com>
2025-02-17 21:43:03 +0530

    Adding notifications functionality

---
+++ QuickBites_Backend/src/main/java/com/quickbites/controller/NotificationController.java
@@ -0,0 +1,37 @@
+package com.quickbites.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quickbites.Exception.UserException;
+import com.quickbites.entities.Notification;
+import com.quickbites.entities.User;
+import com.quickbites.service.NotificationService;
+import com.quickbites.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class NotificationController {
+	
+	@Autowired
+	private NotificationService notificationSerivce;
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/notifications")
+	public ResponseEntity<List<Notification>> findUsersNotification(
+			@RequestHeader("Authorization") String jwt) throws UserException{
+		User user=userService.findUserProfileByJwt(jwt);
+		
+		List<Notification> notifications=notificationSerivce.findUsersNotification(user.getId());
+		return new ResponseEntity<List<Notification>>(notifications,HttpStatus.ACCEPTED);
+	}
+
+}
