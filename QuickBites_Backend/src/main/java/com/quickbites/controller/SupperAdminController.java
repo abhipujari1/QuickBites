cd02388b135b03457803fe1b6d320114eb0839ff
Abhishek <abhikpujari7@gmail.com>
2025-02-17 21:51:48 +0530

    Adding admin specific api's

--- QuickBites_Backend/src/main/java/com/quickbites/controller/SupperAdminController.java
+++ QuickBites_Backend/src/main/java/com/quickbites/controller/SupperAdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.quickbites.entities.User;
-import com.quickbites.repository.UserRepository;
+
 import com.quickbites.service.UserService;
 
 @RestController
